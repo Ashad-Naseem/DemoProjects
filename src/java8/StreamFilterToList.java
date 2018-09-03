15dc3e8c836177964e811b56861d31e7c47d3c83
Ashad Naseem <ashad.naseem@soprasteria.com>
2018-09-03 12:08:06 +0530

    First commit

---
+++ src/java8/StreamFilterToList.java
@@ -0,0 +1,17 @@
+package java8;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamFilterToList {
+
+    public static void main(String[] args) {
+
+        Stream<Integer> number = Stream.of(1, 2, 3, 4, 5);
+
+        List<Integer> result2 = number.filter(x -> x!= 3).collect(Collectors.toList());
+
+        result2.forEach(x -> System.out.println(x));
+    }
+}
